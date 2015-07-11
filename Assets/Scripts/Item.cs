edd5e42368f693e2f516a309615a2e95f18679f6
cullenohara <cullenohara@gmail.com>
2015-07-11 14:06:00 -0500

    Inventory Setup

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour {
+
+	public enum ItemType { tool, material, consumable, armor, weapon }
+
+	public string itemName;
+	public ItemType type;
+	public int stackMax;
+	public Sprite spriteNeutral;
+	public Sprite spriteHighlighted;
+
+	public void Use ()
+	{
+		switch (type) 
+		{
+		case ItemType.consumable:
+			Debug.Log("You just used " + itemName);
+			break;
+		}
+	}
+}
---
+++ Assets/Scripts/Slot.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class Slot : MonoBehaviour {
+
+	private Stack<Item> stack;
+	public Text stackText;
+	public Sprite slotEmpty;
+	public Sprite slotHighlighted;
+
+	// Use this for initialization
+	void Start () {
+		stack = new Stack<Item> ();
+		RectTransform slotRect = GetComponent<RectTransform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
